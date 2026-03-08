daa0b2470b566063f52ef9a68b1612e8ca2d8868
pa-nuzz <94284238+pa-nuzz@users.noreply.github.com>
2026-03-08 22:16:31 +0545

    Auth system: login, register, forgot password

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -9,15 +9,23 @@ class User(AbstractUser):
 
 
 
+from django.contrib.auth.hashers import make_password, check_password
+
 class SMTPProfile(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     email = models.EmailField(unique=True)
     host = models.CharField(max_length=255)
     port = models.IntegerField(default=587)
     username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255) # NOTE: Dev A needs to encrypt this!
+    password = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     daily_limit = models.IntegerField(default=100)
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
 
     def __str__(self):
         return self.email
